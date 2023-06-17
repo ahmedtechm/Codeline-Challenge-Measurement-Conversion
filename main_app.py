544e3e7c9c4110a11437c00cde22de9277a022e6
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-17 17:03:43 +0400

    Updated Measurement Conversion codes

--- main_app.py
+++ main_app.py
@@ -5,30 +5,37 @@ app = Flask(__name__)
 
 def convert_measurements(input_string):
     measurements = input_string.split(',')
-    total_values = []
-
+    result = []
     for measurement in measurements:
-        values = measurement.split(' ')
-        total = 0
+        count = ""
+        values = []
+        for char in measurement:
+            if char.isdigit():
+                count += char
+            else:
+                if count != "":
+                    values.append(int(count))
+                    count = ""
+                if char.isalpha():
+                    value = ord(char.lower()) - ord('a') + 1
+                    values.append(value)
+        if count != "":
+            values.append(int(count))
+        result.append(sum(values))
+    return result
 
-        for value in values:
-            try:
-                num = float(value)
-                total += num
-            except ValueError:
-                pass
 
-        total_values.append(total)
-
-    return total_values
-
-
-@app.route('/convert', methods=['POST'])
-def convert():
-    data = request.get_json()
-    input_string = data['input_string']
-    result = convert_measurements(input_string)
-    return jsonify({'result': result})
+@app.route('/')
+def convert_measurements_api():
+    input_string = request.args.get('convert-measurements')
+    if input_string:
+        try:
+            result = convert_measurements(input_string)
+            return jsonify({'result': result})
+        except Exception as e:
+            return jsonify({'error': str(e)})
+    else:
+        return jsonify({'error': 'Missing query parameter "convert-measurements"'})
 
 
 if __name__ == '__main__':
